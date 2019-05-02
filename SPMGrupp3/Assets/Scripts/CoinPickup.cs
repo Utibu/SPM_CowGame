1f39bf040eaee6eb7ccc798143afe49043223943
Jocke <jockeljung1@gmail.com>
2019-05-02 15:02:35 +0200

    Added "coin" pickups

---
+++ SPMGrupp3/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : DroppableObject
+{
+    //public GameObject player;
+
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        GameManager.instance.coinCount++;        
+
+    }
+}
