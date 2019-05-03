16b048083f1698409260c7e420fde68e54b173df
Niklas Almqvist <git@utibu.se>
2019-05-03 11:22:26 +0200

    Inputmanager

---
+++ SPMGrupp3/Assets/Scripts/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    public bool EventKeyPressed()
+    {
+        if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
