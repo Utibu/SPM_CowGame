ca10f54541548eb5d496298685f4b4fbd6694852
Niklas Almqvist <git@utibu.se>
2019-05-03 09:24:26 +0200

    Objekt är tillbaka

---
+++ SPMGrupp3/Assets/IMPORTANTE STUFF/RollingBallState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RollingBallStateMachine/RollingBallState")]
+public class RollingBallState : PhysicsBaseState
+{
+    private float time;
+    public float rockVelocityToHurtPlayer = 4f;
+
+    public override void Enter()
+    {
+        base.Enter();
+        //((RollingBallStateMachine)owner).moveMultiplier = moveMultiplier;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        time += Time.deltaTime;
+
+        if ((time % 60 > 10 && owner.velocity.magnitude < 1f) || time % 60 > 120)
+        {
+            Destroy(owner.gameObject);
+        }
+    }
+
+    public override void Leave()
+    {
+        base.Leave();
+    }
+
+    public override void ActOnCollision(Collider hitCollider)
+    {
+        if(hitCollider.tag.Equals("Player"))
+        {
+            Debug.LogWarning("PLAYERDASH:" + GameManager.instance.player.velocity.magnitude);
+        }
+            
+        if (hitCollider.tag.Equals("Player") && owner.velocity.magnitude > rockVelocityToHurtPlayer)
+        {
+            
+            GameManager.instance.player.playerValues.health -= 25;
+            Destroy(owner.gameObject);
+        }
+    }
+}
