710135008c23123724ec39ae90ec4bd3698b6fcd
maxdahl777 <49674130+maxdahl777@users.noreply.github.com>
2019-05-05 14:54:05 +0200

    lvl 02

---
+++ SPMGrupp3/Assets/AudioAssets/AudioScript/BirdChirpScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdChirpScript : MonoBehaviour
+{
+
+    private AudioSource source;
+    public AudioClip clip;
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = true;
+
+        source.time = Random.Range(0, clip.length);
+
+        source.Play();
+    }
+}
+
+
