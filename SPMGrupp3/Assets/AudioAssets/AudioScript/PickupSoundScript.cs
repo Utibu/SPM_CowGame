2cd25567ebd96f4d891b1857c8f18b21dd1b226d
Znigmrak <38332118+Znigmrak@users.noreply.github.com>
2019-05-07 10:27:10 +0200

    Sound script

---
+++ SPMGrupp3/Assets/AudioAssets/AudioScript/PickupSoundScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSoundScript : MonoBehaviour
+{
+
+    private AudioSource[] sources;
+    private AudioSource source1;
+    public AudioClip[] pick_up_sounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sources = GetComponents<AudioSource>();
+        source1 = sources[0];
+    }
+
+    if (other.gameObject.CompareTag ("Droppable") ){
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+
+    clipIndex = Random.Range(0, pick_up_sounds.Length);
+            source1.PlayOneShot(pick_up_sounds[clipIndex]);
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
