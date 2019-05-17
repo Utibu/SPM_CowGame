996c1939fe19638d48d58c7d75dabf32f5c88902
Barburat <38035810+Barburat@users.noreply.github.com>
2019-05-17 14:46:36 +0200

    Boss moves to snipe location over time

--- SPMGrupp3/Assets/Scripts/States/Boss/BossSnipeState.cs
+++ SPMGrupp3/Assets/Scripts/States/Boss/BossSnipeState.cs
@@ -9,46 +9,58 @@ public class BossSnipeState : BossBaseState
     private Vector3 originalPosition;
     [SerializeField] private float attackSpeed;
     private float originalAttackSpeed;
+    private float rangeFromDestination = 3f;
 
     public override void Enter()
     {
-        Debug.Log("in snipe");
         base.Enter();
         originalPosition = owner.transform.position;
         //owner.bulletsShotSinceReload = 0;
-        owner.agnes.isStopped = true;
-        owner.agnes.Warp(owner.snipeLocation.transform.position);
+        //owner.agnes.Warp(owner.snipeLocation.transform.position);
+        //owner.agnes.isStopped = true;
+        owner.agnes.enabled = false;
         originalAttackSpeed = owner.attackSpeed;
         owner.attackSpeed = attackSpeed;
+
 
     }
 
     public override void Update()
     {
-        base.Update();
         lookAt();
 
-        countdown -= Time.deltaTime;
-        if (countdown <= 0)
+        //if (Mathf.Sqrt(owner.transform.position.sqrMagnitude - owner.snipeLocation.transform.position.sqrMagnitude) >= rangeFromDestination)
+        if(!Helper.IsWithinDistance(owner.transform.position, owner.snipeLocation.transform.position, rangeFromDestination))
         {
-            attack();
-            countdown = attackSpeed;
+            owner.transform.position = Vector3.Slerp(owner.transform.position, owner.snipeLocation.transform.position, 1.5f * Time.deltaTime);
 
         }
-        /*
-        if (owner.bulletsShotSinceReload >= owner.bulletsBeforeReload)
+        else
         {
-            owner.bulletsShotSinceReload = 0;
-            owner.Transition<BossAttackState>();
+            owner.agnes.enabled = true;
+
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                attack();
+                countdown = attackSpeed;
+
+            }
+            /*
+            if (owner.bulletsShotSinceReload >= owner.bulletsBeforeReload)
+            {
+                owner.bulletsShotSinceReload = 0;
+                owner.Transition<BossAttackState>();
+            }
+            */
         }
-        */
     }
 
     public override void Leave()
     {
-        Debug.Log("leaving snipe");
         base.Leave();
-        owner.agnes.isStopped = false;
+        //owner.agnes.isStopped = false;
+        //owner.agnes.enabled = true;
         owner.agnes.Warp(originalPosition);
         owner.bulletsShotSinceReload = 0;
         
