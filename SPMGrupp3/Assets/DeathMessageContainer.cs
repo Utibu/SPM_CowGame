33eedfdc29a6d1a9b2159ba4fd14edfa4624f877
Barburat <38035810+Barburat@users.noreply.github.com>
2019-05-22 10:58:24 +0200

    Death screen now works

--- SPMGrupp3/Assets/DeathMessageContainer.cs
+++ SPMGrupp3/Assets/DeathMessageContainer.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class DeathMessageContainer : MonoBehaviour
 {
-
+    //[SerializeField] private PlayerStateMachine player;
+    //private PlayerStateMachine stateMachine;
+    private MeshRenderer playerMeshRenderer;
 
     void Start()
     {
-        
+
+    }
+
+    private void Awake()
+    {
+        playerMeshRenderer = GameManager.instance.player.GetComponentInChildren<MeshRenderer>();
     }
 
     void Update()
@@ -23,20 +30,38 @@ public class DeathMessageContainer : MonoBehaviour
         gameObject.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        GameManager.instance.Pause();
+        GameManager.instance.player.GetComponentInChildren<MeshRenderer>().enabled = false;
+        UIManager.instance.HideHUD();
+
     }
 
     public void Hide()
     {
         gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void RespawnButtonClicked()
     {
-
+        GameManager.instance.player.GetComponentInChildren<MeshRenderer>().enabled = true;
+        GameManager.instance.Resume();
+        if (LevelManager.instance.currentCheckpoint != null)
+        {
+            GameManager.instance.player.Respawn(LevelManager.instance.currentCheckpoint.transform.position);
+        }
+        else
+        {
+            GameManager.instance.player.Respawn(LevelManager.instance.originalSpawnTransform.position);
+        }
+        UIManager.instance.HideDeathMessage();
+        UIManager.instance.ShowHUD();
     }
 
     public void MainMenuButton()
     {
-
+        SceneManager.LoadScene(0);
     }
+
 }
