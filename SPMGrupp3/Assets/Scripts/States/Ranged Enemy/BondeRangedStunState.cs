fc5ab731f394eb96eec6ab1c412b6f6602c9cdbe
Niklas Almqvist <git@utibu.se>
2019-05-22 13:51:10 +0200

    Ranged ska gå att gå igenom nu

--- SPMGrupp3/Assets/Scripts/States/Ranged Enemy/BondeRangedStunState.cs
+++ SPMGrupp3/Assets/Scripts/States/Ranged Enemy/BondeRangedStunState.cs
@@ -10,13 +10,14 @@ public class BondeRangedStunState : BondeRangedBaseState
     public override void Enter()
     {
         base.Enter();
-        owner.isDying = false;
-        //owner.transform.Rotate(0, 0, 90);
-        owner.transform.position -= new Vector3(0f, owner.StunLengthToGround, 0f);
         time = 0.0f;
+        owner.isDying = false;
         owner.agnes.isStopped = true;
         owner.agnes.enabled = false;
         owner.GetComponent<Collider>().enabled = false;
+        owner.GetComponent<MeshRenderer>().material.color = Color.black;
+        //owner.transform.Rotate(0, 0, 90);
+        owner.transform.position -= new Vector3(0f, owner.StunLengthToGround, 0f); // independant of size, enemy should lie on ground, not float above it.
     }
 
     public override void Update()
@@ -36,12 +37,11 @@ public class BondeRangedStunState : BondeRangedBaseState
     public override void Leave()
     {
         base.Leave();
+        owner.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        owner.transform.position -= new Vector3(0f, -owner.StunLengthToGround, 0f);
         owner.agnes.enabled = true;
         owner.GetComponent<Collider>().enabled = true;
         owner.agnes.isStopped = false;
-        owner.transform.Rotate(0, 0, -90);
-        owner.transform.position -= new Vector3(0f, -owner.StunLengthToGround, 0f);
         owner.DoingKnockback = false;
-        owner.transform.eulerAngles = new Vector3(0f, 0f, 0f);
     }
 }
