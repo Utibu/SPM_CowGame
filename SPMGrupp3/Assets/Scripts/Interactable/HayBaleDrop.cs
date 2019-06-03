7cfacbefdddf19ac2798cfee4dc741f708f362bb
Barburat <38035810+Barburat@users.noreply.github.com>
2019-06-03 10:59:09 +0200

    haybale save fix

--- SPMGrupp3/Assets/Scripts/Interactable/HayBaleDrop.cs
+++ SPMGrupp3/Assets/Scripts/Interactable/HayBaleDrop.cs
@@ -4,35 +4,8 @@ using UnityEngine;
 
 public class HayBaleDrop : DroppableObject
 {
-    
-    public float healthPoints;
-
-
-    // Start is called before the first frame update
-    override public void Start()
+        override public void Start()
     {
-        //EventSystem.Current.RegisterListener<OnPlayerDiedEvent>(ResetHay);
-        //EventSystem.Current.RegisterListener<UnregisterListenerEvent>(Unregister);
         GameManager.instance.SaveManager.Haybales.Add(GetComponent<Saveable>().Id, gameObject);
     }
-
-    public override void OnPlayerTriggerEnter(Collider hitCollider)
-    {
-        //Debug.Log("haybaledrop");
-        //base.OnPlayerTriggerEnter(hitCollider);
-        //Debug.Log("HAYBALE");
-        //player.health += healthPoints;
-        
-    }
-
-    private void ResetHay(OnPlayerDiedEvent playerDiedEvent)
-    {
-        //gameObject.SetActive(true);
-    }
-
-    private void Unregister(UnregisterListenerEvent eventInfo)
-    {
-        //EventSystem.Current.UnregisterListener<OnPlayerDiedEvent>(ResetHay);
-    }
-    
 }
